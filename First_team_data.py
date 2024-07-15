2c08d6052e5a64488ced8251b3713fc60209d83c
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-15 07:51:47 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -260,6 +260,9 @@ def Dashboard():
         st.dataframe(df_matchstats_tabel, hide_index=True)
         # Beregn 3-kamps rullende gennemsnit for hver team
         
+        df_matchstats = df_matchstats[df_matchstats['openPlayPass'] != 'NaN']
+
+        
         df_matchstats['rolling_openPlayPass'] = df_matchstats.groupby('team_name')['openPlayPass'].transform(lambda x: x.rolling(3, min_periods=1).mean())
         df_matchstats['rolling_successfulOpenPlayPass'] = df_matchstats.groupby('team_name')['successfulOpenPlayPass'].transform(lambda x: x.rolling(3, min_periods=1).mean())
 
