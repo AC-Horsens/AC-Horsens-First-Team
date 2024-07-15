af684bf9fb14071a07fb377080732d4d702a65af
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-15 12:43:37 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -900,7 +900,7 @@ def League_stats():
             team_rank = team_df[col + '_rank'].values[0]
             if selected_rank in target_ranks and team_rank in target_ranks:
                 count_matches += 1
-            if count_matches >= 3:
+            if count_matches >= 5:
                 similar_teams.append(team)
                 break
 
