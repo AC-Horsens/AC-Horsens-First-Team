6d22601d2ddcd31d876d6298e37926e607c24d44
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-15 08:10:24 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -265,7 +265,7 @@ def Dashboard():
         df_matchstats['rolling_openPlayPass'] = df_matchstats.groupby('team_name')['openPlayPass'].transform(lambda x: x.rolling(3, min_periods=1).mean())
         df_matchstats['rolling_successfulOpenPlayPass'] = df_matchstats.groupby('team_name')['successfulOpenPlayPass'].transform(lambda x: x.rolling(3, min_periods=1).mean())
 
-        df_matchstats = df_matchstats.dropna(subset=['rolling_openPlayPass', 'rolling_successfulOpenPlayPass'])
+        df_matchstats = df_matchstats.dropna(subset=['rolling_openPlayPass', 'rolling_successfulOpenPlayPass','date'])
 
         fig1 = go.Figure()
 
