884e956488965c15eea3bc8e194ab991a00666a7
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-15 10:54:23 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -824,7 +824,7 @@ def League_stats():
     matchstats_df = matchstats_df.groupby(['contestantId', 'team_name']).sum().reset_index()
     matchstats_df = matchstats_df.rename(columns={'label': 'matches'})
     matchstats_df = matchstats_df.merge(df_spacecontrol)
-
+    st.dataframe(matchstats_df)
     matchstats_df['PenAreaEntries per match'] = matchstats_df['penAreaEntries'] / matchstats_df['matches']
     matchstats_df['Open play xG per match'] = matchstats_df['open play xG'] / matchstats_df['matches']
     matchstats_df['Duels per match'] = (matchstats_df['duelLost'] + matchstats_df['duelWon']) /matchstats_df['matches']
