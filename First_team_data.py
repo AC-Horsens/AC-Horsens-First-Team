8c362e268bdada4388c3e2ca509beeec1458e5a7
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-16 11:18:26 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -612,6 +612,7 @@ def Dashboard():
         st.write('Early crosses')
         df_crosses = df_crosses[df_crosses['label'].isin(match_choice)]
         df_early_crosses = df_crosses[(df_crosses['x'].astype(float) <= 88.5) & ((df_crosses['y'].astype(float) >= 78.9) | (df_crosses['y'].astype(float) <= 21.1))]
+        st.write(df_early_crosses.columns)
         st.dataframe(df_early_crosses, hide_index=True)
     def pressing():
         df_possession_data = load_possession_data()
