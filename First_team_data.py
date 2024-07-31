59bb3713abc2035168154dcc2893ea675483c12f
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-31 13:27:16 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2168,7 +2168,6 @@ def League_stats():
 def Physical_data():
     df = load_physical_data()
     df.set_index('Team', inplace=True)
-    st.write(df.dtypes)
     df = df.applymap(lambda x: f"{x:.2f}" if isinstance(x, float) else f"{x}")
 
     st.dataframe(df)
