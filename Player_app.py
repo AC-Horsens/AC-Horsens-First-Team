44d4e9376d2378d1577798bb59b7327ebe4fe99a
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-19 15:27:59 +0200

    Update Player_app.py

--- Player_app.py
+++ Player_app.py
@@ -808,7 +808,7 @@ classic_striker_df = position_dataframes['Classic striker']
     
 def player_data(df_possession_data,df_matchstats,balanced_central_defender_df,fullbacks_df,number8_df,number6_df,number10_df,winger_df,classic_striker_df):
     team_name = st.selectbox('Choose team', team_names = ['B_93','Esbjerg','Fredericia','HB_Køge','Hillerød','Hobro','Horsens','Hvidovre','Kolding','OB','Roskilde','Vendsyssel'])
-    horsens = df_possession_data.copy
+    horsens = df_possession_data.copy()
     horsens = df_possession_data[df_possession_data['team_name'].str.contains(team_name)]
     horsens = horsens.sort_values(by='playerName')
     player_name = st.selectbox('Choose player', horsens['playerName'].unique())
