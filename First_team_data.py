397b904f11a927effe7726ef4230162c44052e47
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-29 08:37:06 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2313,7 +2313,7 @@ def League_stats_superliga():
     matchstats_df['Open play shot assists share'] = matchstats_df['attAssistOpenplay'] / matchstats_df['totalAttAssist']
     matchstats_df['Crosses'] = matchstats_df['totalCrossNocorner']
     matchstats_df['Cross accuracy %'] = matchstats_df['accurateCrossNocorner'] / matchstats_df['totalCrossNocorner']
-    matchstats_df = matchstats_df[['team_name','matches','PenAreaEntries per match','Open play xG per match','Duels per match','Duels won %','Passes per game','Pass accuracy %','Back zone pass accuracy %','Forward zone pass accuracy %','possWonDef3rd %','possWonMid3rd %','possWonAtt3rd %','Forward pass share %','Final third entries per match','Final third pass accuracy %','Open play shot assists share','Long pass share %','Crosses','Cross accuracy %']]
+    matchstats_df = matchstats_df[['team_name','matches','PenAreaEntries per match','Open play xG per match','Duels per match','Duels won %','Passes per game','Pass accuracy %','Back zone pass accuracy %','Forward zone pass accuracy %','possWonDef3rd %','possWonMid3rd %','possWonAtt3rd %','Forward pass share %','Final third entries per match','Final third pass accuracy %','Open play shot assists share','Crosses','Cross accuracy %']]
 
     cols_to_rank = matchstats_df.drop(columns=['team_name']).columns
     ranked_df = matchstats_df.copy()  # Create a copy of the original DataFrame
