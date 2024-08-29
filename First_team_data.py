bfd5b680dbe5e1c2c161fc99b36cddfbb890d996
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-29 15:03:36 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2310,7 +2310,7 @@ def League_stats():
     # Optional: Remove the duplicate xG column if you only need the sequence_xg
     df_corners_for = df_corners_for.drop(columns=['321.0_corner'])
     st.dataframe(df_corners_for)
-    inswingers = df_corners_for[df_corners_for['223.0'] == True].groupby(['team_name', 'playerName']).size().reset_index(name='inswingers')
+    inswingers = df_corners_for[df_corners_for['223.0'] == True]
     outswingers = df_corners_for[df_corners_for['224.0'] == True].groupby(['team_name', 'playerName']).size().reset_index(name='outswingers')
     inswingers = inswingers.groupby(['team_name', 'playerName']).mean('sequence_xg').reset_index()
     st.dataframe(inswingers)
