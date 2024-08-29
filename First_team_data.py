5594b4bc759eaedf106830852d31f6fc553259ea
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-29 14:36:49 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2314,7 +2314,7 @@ def League_stats():
     outswingers = df_corners_for[df_corners_for['224.0'] == True].groupby(['team_name', 'playerName']).size().reset_index(name='outswingers')
 
     # Merge inswingers and outswingers counts
-    df_corners_for = pd.merge(inswingers, outswingers, on=['team_name', 'playerName'])
+    df_corners_for = pd.merge(inswingers, outswingers, on=['team_name', 'playerName'],how='outer').fillna(0)
 
     # Fill NaN values with 0 for cases where a player might not have inswingers or outswingers
     df_corners_for = df_corners_for.fillna(0)
