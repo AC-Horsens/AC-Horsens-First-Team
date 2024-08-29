56190ff1f2776677e8bc3efcddd57eb2605a8946
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-29 15:03:57 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2312,7 +2312,6 @@ def League_stats():
     st.dataframe(df_corners_for)
     inswingers = df_corners_for[df_corners_for['223.0'] == True]
     outswingers = df_corners_for[df_corners_for['224.0'] == True].groupby(['team_name', 'playerName']).size().reset_index(name='outswingers')
-    inswingers = inswingers.groupby(['team_name', 'playerName']).mean('sequence_xg').reset_index()
     st.dataframe(inswingers)
     df_corners_for = pd.merge(df_corners_for, inswingers, on=['team_name', 'playerName'])
     df_corners_for = pd.merge(df_corners_for, outswingers, on=['team_name', 'playerName']).fillna(0)
