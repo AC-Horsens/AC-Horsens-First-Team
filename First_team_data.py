6af72f14461dc913ef13ed7b263c9a6c36177fbc
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-29 13:58:43 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -1858,6 +1858,7 @@ def Dashboard():
 
         # Display the DataFrame in Streamlit
         corner_xg = corner_xg.merge(freekick_xg, on='team_name', how='left')
+        corner_xg['Set piece xg'] = corner_xg['corner xg'] + corner_xg['freekick xg']
         st.dataframe(corner_xg)
         df_xg = load_all_xg()
     Data_types = {
