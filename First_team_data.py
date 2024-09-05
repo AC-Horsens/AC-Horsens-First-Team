743eb71eb86b8e35e37985e914c97a4974581acf
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-09-05 12:06:03 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2328,7 +2328,7 @@ def League_stats():
     df_short = df_short[df_short['team_name'] == selected_team]
     df_short['sequence_xg'] = df_short.groupby(['sequenceId', 'playerName'])['321.0_full'].transform('first')
     df_short['sequence_xg'] = df_short['sequence_xg'].fillna(0)
-    df_short_for_plot = df_short[(df_short['6.0'] == True) & df_short['212'] < 15]
+    df_short_for_plot = df_short[(df_short['6.0'] == True) & df_short['212.0'] < 15]
     df_short_for_plot = df_short_for_plot[['playerName','sequenceId', 'x', 'y', '140.0', '141.0','321.0_full','sequence_xg']]
     
     
