2eccb61d83c2f8401cba81f570a1479c09617021
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-09-05 10:32:07 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2070,6 +2070,7 @@ def League_stats():
     matchstats_df['Center Space control %'] = matchstats_df['Center Control Area %']
     matchstats_df['Penalty Area Space control %'] = matchstats_df['Penalty Area Control %']
     matchstats_df = matchstats_df[['team_name','matches','PenAreaEntries per match','Open play xG per match','Duels per match','Duels won %','Passes per game','Pass accuracy %','Back zone pass accuracy %','Forward zone pass accuracy %','possWonDef3rd %','possWonMid3rd %','possWonAtt3rd %','Forward pass share %','Final third entries per match','Final third pass accuracy %','Open play shot assists share','PPDA per match','Total Space control %','Center Space control %','Penalty Area Space control %','Long pass share %','Crosses','Cross accuracy %']]
+    matchstats_df['team_name'] = matchstats_df['team_name'].str.replace(' ', '_')
 
     cols_to_rank = matchstats_df.drop(columns=['team_name']).columns
     ranked_df = matchstats_df.copy()  # Create a copy of the original DataFrame
