3d4d36c6ff84de4ecffa13bb0b024024fef644f4
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-09-05 12:18:27 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2414,12 +2414,12 @@ def League_stats():
     for _, row in df_short_for_plot_right.iterrows():
         x_start, y_start = row['x'], row['y']
         x_end, y_end = row['140.0'], row['141.0']
-        pitch_short_right.arrows(x_start, y_start, x_end, y_end, width=2, headwidth=5, headlength=5, color='blue', ax=axs[0, 0])
+        pitch_short_right.arrows(x_start, y_start, x_end, y_end, width=2, headwidth=5, headlength=5, color='blue', ax=axs[0, 3])
 
     for _, row in df_short_for_plot_left.iterrows():
         x_start, y_start = row['x'], row['y']
         x_end, y_end = row['140.0'], row['141.0']
-        pitch_short_left.arrows(x_start, y_start, x_end, y_end, width=2, headwidth=5, headlength=5, color='blue', ax=axs[1, 0])
+        pitch_short_left.arrows(x_start, y_start, x_end, y_end, width=2, headwidth=5, headlength=5, color='blue', ax=axs[1, 3])
 
     axs[0, 3].set_title("Short - Left Side")
     axs[1, 3].set_title("Short - Right Side")
