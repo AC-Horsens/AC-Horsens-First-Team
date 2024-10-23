a084ed2ef992b934880bfe3c6fcc86edfab543f3
Seamus1992 <spbartholdy@gmail.com>
2024-10-23 13:40:42 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -1456,7 +1456,9 @@ def Dashboard():
         st.header('Touches in box')
         st.write('Whole season')
         touches_in_box_player = df_matchstats[df_matchstats['team_name'] == 'Horsens']
+
         touches_in_box_player = touches_in_box_player[touches_in_box_player['label'].isin(match_choice)]
+        st.write(touches_in_box_player)
         touches_in_box_player = touches_in_box_player.groupby(['playerName'])['touchesInOppBox'].sum().reset_index()
         touches_in_box_player = touches_in_box_player.sort_values(by=['touchesInOppBox'], ascending=False)
         touches_in_box_team = df_matchstats.groupby(['team_name','date', 'label'])['touchesInOppBox'].sum().reset_index()
