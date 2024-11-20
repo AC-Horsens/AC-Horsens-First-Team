316acaf36d898508f1ced8f16615f80994d1f476
Seamus1992 <spbartholdy@gmail.com>
2024-11-20 14:46:40 +0100

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2585,10 +2585,10 @@ def Opposition_analysis():
             players = position_data.get(position, [])
             for x, y in coords:
                 # Add position title
-                plt.text(x, y + 5, position, fontsize=10, ha="center", color="blue", fontweight="bold")
+                plt.text(x, y + 3, position, fontsize=10, ha="center", color="blue", fontweight="bold")
                 # Display the same list of players for both locations
                 for j, (player_name, total_score) in enumerate(players):
-                    offset_y = y - j * 1  # Adjust vertical offset for each player
+                    offset_y = y - j * 2  # Adjust vertical offset for each player
                     plt.text(x, offset_y, f"{player_name} ({total_score})", fontsize=8, ha="center", color="black")
 
         # Remove axes for a clean look
