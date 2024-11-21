5f08b64d836b03a57a8ad7704fa3add59ee0e106
Seamus1992 <spbartholdy@gmail.com>
2024-11-21 10:06:00 +0100

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2129,7 +2129,7 @@ def Opposition_analysis():
     df_ppda['date'] = pd.to_datetime(df_ppda['date'])
     df_ppda['PPDA'] = df_ppda['PPDA'].astype(float).round(2)
     df_ppda = df_ppda[['team_name','date', 'PPDA']]
-    matchstats_df = xg_df_openplay.merge(filtered_data,on=['contestantId','team_name','date'],how='right')
+    matchstats_df = xg_df_openplay.merge(filtered_data,on=['contestantId','team_name','date'],how='inner')
     st.dataframe(matchstats_df)
     matchstats_df = df_ppda.merge(matchstats_df)
 
