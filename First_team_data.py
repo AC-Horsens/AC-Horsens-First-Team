59efb79717d035e20c10a567c4182a41715e15a6
Seamus1992 <spbartholdy@gmail.com>
2024-11-25 13:46:20 +0100

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -893,11 +893,12 @@ def Process_data_spillere(df_xA,df_pv_all,df_match_stats,df_xg_all,squads):
         df_striker = calculate_score(df_striker, 'xA_per90', 'xA per90 score')
         df_striker = calculate_score(df_striker, 'attemptsIbox_per90', 'Attempts in box per90 score')
         df_striker = calculate_score(df_striker, 'xg_per90', 'xG per90 score')
+        df_striker = calculate_score(df_striker, 'post_shot_xg_per90', 'Post shot xG per90 score')
 
         # Combine scores into categories
         df_striker['Linkup play'] = df_striker[['Forward zone pass % score', 'Passing % score', 'Possession value score']].mean(axis=1)
-        df_striker['Chance creation'] = df_striker[['attAssistOpenplay_per90 score', 'penAreaEntries_per90 score']].mean(axis=1)
-        df_striker['Goalscoring'] = df_striker[['xg_per90 score', 'Touches in box per90 score']].mean(axis=1)
+        df_striker['Chance creation'] = df_striker[['Open play assists score', 'Penalty area entries score']].mean(axis=1)
+        df_striker['Goalscoring'] = df_striker[['xg_per90 score','xg_per90 score', 'xg_per90 score','Post shot xG per90 score', 'Touches in box per90 score']].mean(axis=1)
         df_striker['Possession value'] = df_striker[['Possession value total score', 'possessionValue.pvAdded_per90']].mean(axis=1)
 
         # Calculate component scores
