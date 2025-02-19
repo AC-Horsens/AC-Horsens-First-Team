9aed31c7f6ac10bca7f48683055284ff4d58353d
Seamus1992 <spbartholdy@gmail.com>
2025-02-19 13:44:57 +0100

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2033,6 +2033,7 @@ def Dashboard():
         df_set_pieces = df_set_pieces.round(2)
         df_set_pieces_goals = df_set_pieces[df_set_pieces['typeId'] == 16]
         df_set_pieces_goals = df_set_pieces_goals.groupby('team_name').size().reset_index(name='Goals')
+        df_set_pieces_goals = df_set_pieces_goals.sort_values(by='Goals',ascending=False)
         df_set_pieces_matches = df_set_pieces.groupby(['team_name','label']).agg({'321.0':'sum'}).reset_index()
         df_set_pieces_matches['xG_match'] = df_set_pieces_matches.groupby('label')['321.0'].transform('sum')
         df_set_pieces_matches['xG_against'] = df_set_pieces_matches['321.0'] - df_set_pieces_matches['xG_match']
