dbea8763d035040a6392a12ca2f07023d2c9bca5
Seamus1992 <spbartholdy@gmail.com>
2025-04-17 15:12:55 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -1314,11 +1314,11 @@ def Dashboard():
     def defensive_line_data():
         def_line = load_def_line_data()
         labels_df = df_possession[['match_id','date', 'label']].drop_duplicates()
-        st.write(labels_df)
         states_df = df_possession[['match_id', 'contestantId', 'timeMin', 'timeSec', 'match_state']]
 
         # Merge only on match_id to get label
         def_line = def_line.merge(labels_df, on='match_id', how='left')
+        st.write(labels_df)
 
         # Merge on full key to get match_state
         def_line = def_line.merge(states_df, on=['match_id', 'contestantId', 'timeMin', 'timeSec'], how='left')
