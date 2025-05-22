9b1f26dd8fdc7a63eb3d99f0f69cd8269f9c9141
Seamus1992 <spbartholdy@gmail.com>
2025-05-22 14:09:29 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -1263,6 +1263,8 @@ def Dashboard():
     def transitions():
         df_transitions = load_transitions_data()
         df_transitions['date'] = pd.to_datetime(df_transitions['date'])
+        df_transitions = df_transitions.apply(pd.to_numeric, errors='coerce')
+
         df_transitions = df_transitions.merge(df_possession,how='left')
         st.dataframe(df_transitions)
 
