f6b5c903290b82c1b4cef4875e8d675c72a78339
Seamus1992 <spbartholdy@gmail.com>
2025-06-16 16:44:30 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -1468,7 +1468,7 @@ def Dashboard():
         )
 
         assist_zone_possessions = df_transitions[zone1_mask]
-
+        st.dataframe(assist_zone_possessions)
         # --- Count assist zone actions ---
         assist_zone_counts = assist_zone_possessions.groupby('team_name')['id'].count().reset_index(name='Assist zone actions')
 
