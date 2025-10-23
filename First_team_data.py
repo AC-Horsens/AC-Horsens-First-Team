6335e2c5bcba1836e2257b6a5801aea728600ee4
Seamus1992 <spbartholdy@gmail.com>
2025-10-23 10:06:55 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -3350,7 +3350,7 @@ def Opposition_analysis():
 
     for col in selected_columns:
         weighted_df[col] = weighted_df[col] * weights[col]
-    nn = NearestNeighbors(n_neighbors=4, metric="cosine")  # 4 because 1 will be the team itself
+    nn = NearestNeighbors(n_neighbors=5, metric="cosine")  # 4 because 1 will be the team itself
     nn.fit(weighted_df.fillna(0))
 
     # Find the selected team index
