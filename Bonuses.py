9ab63e9b515ddc1183815f46971fc8f96c342e04
Seamus1992 <spbartholdy@gmail.com>
2025-11-13 10:06:33 +0100

    Update Bonuses.py

--- Bonuses.py
+++ Bonuses.py
@@ -70,6 +70,7 @@ merged_df['Starting_11_wins'] = merged_df.apply(
 merged_df['Subbed_in_wins'] = merged_df.apply(
     lambda row: 1 if row['Subbed_in'] > 0 and row['wins'] == True else 0, axis=1
 )
+st.dataframe(merged_df)
 # Print the filtered dataframe
 final_df = (
     merged_df.groupby('playerName')
