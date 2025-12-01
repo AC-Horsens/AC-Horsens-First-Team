950b65a497b8becb5ec893a44ec54bdbdc9a62dc
Seamus1992 <spbartholdy@gmail.com>
2025-12-01 09:41:25 +0100

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -2042,12 +2042,13 @@ def Dashboard():
         st.subheader('Transition starts with shot')
 
         transitions_starts = df_transitions[
-            (df_transitions['possession_index'] == 2) & 
-            (df_transitions['team_name'] == 'Opponent') &
-            (df_transitions['sequence_duration'] >= 0) &
-            (df_transitions['sequence_xG'] >= 0)
+            (df_transitions['possession_index'] == 1) & 
+            (df_transitions['team_name'] == 'Horsens') &
+            (df_transitions['sequence_duration'] > 0) &
+            (df_transitions['sequence_xG'] > 0)
         ]
-
+        action_count = transitions_starts['typeId'].value_counts()
+        st.dataframe(action_count)
         vis_type = st.selectbox("Choose visualization type", ["Pitch Scatter", "Heatmap"])
         plot_transitions(transitions_starts, vis_type)
 
